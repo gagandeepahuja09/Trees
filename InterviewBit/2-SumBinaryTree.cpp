2e60fce8eb71e02c0d88ef4d7939a1a300340c16
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-03-31 15:15:47 +0530

    two pointer iterative inorder and reverse indorder using stack

---
+++ InterviewBit/2-SumBinaryTree.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+// Apply the approach of method of two pointers to BST
+// For that the two stacks will store iterative inorder traversal
+// And reverse iterative inorder traversal
+
+int Solution::t2Sum(TreeNode* A, int B) {
+    if(!A)
+        return 0;
+    stack<TreeNode*> s1, s2;
+    TreeNode *temp1 = A, *temp2 = A;
+    while(temp1) {
+        s1.push(temp1);
+        temp1 = temp1 -> left;
+    }
+    while(temp2) {
+        s2.push(temp2);
+        temp2 = temp2 -> right;
+    }
+    temp1 = s1.top();
+    temp2 = s2.top();
+    while(temp1 && temp2 && temp1 -> val < temp2 -> val) {
+        if(temp1 -> val + temp2 -> val == B)
+            return 1;
+        else if(temp1 -> val + temp2 -> val < B) {
+            s1.pop();
+            temp1 = temp1 -> right;
+            while(temp1) {
+                s1.push(temp1);
+                temp1 = temp1 -> left;
+            }
+            temp1 = s1.top();
+        }
+        else {
+            s2.pop();
+            temp2 = temp2 -> left;
+            while(temp2) {
+                s2.push(temp2);
+                temp2 = temp2 -> right;
+            }
+            temp2 = s2.top();
+        }
+    }
+    return 0;
+}
+
