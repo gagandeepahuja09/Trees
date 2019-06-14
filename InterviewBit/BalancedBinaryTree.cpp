36a4a906c25e3abcc2f6ccea69f4ebcc0dae9f57
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-06-14 12:28:02 +0530

    easy

--- InterviewBit/BalancedBinaryTree.cpp
+++ InterviewBit/BalancedBinaryTree.cpp
@@ -7,21 +7,21 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-
-pair<bool, int> balanceHeight(TreeNode* A) {
+ 
+pair<bool, int> balance(TreeNode *A) {
     if(!A) {
         return { true, 0 };
     }
-    pair<bool, int> l = balanceHeight(A -> left);
-    pair<bool, int> r = balanceHeight(A -> right);
-    bool ans = true;
-    if((!l.first || !r.first) || (abs(l.second - r.second) > 1))
-        ans = false;
-    int h = max(l.second, r.second) + 1;
-    return { ans, h };
-} 
+    pair<bool, int> l = balance(A -> left);
+    pair<bool, int> r = balance(A -> right);
+    bool ans = false;
+    int lh = l.second, rh = r.second;
+    if(abs(lh - rh) <= 1 && l.first && r.first)
+        ans = true;
+    return { ans, max(lh, rh) + 1 };    
+}
  
 int Solution::isBalanced(TreeNode* A) {
-    return balanceHeight(A).first;
+    return balance(A).first;
 }
 
