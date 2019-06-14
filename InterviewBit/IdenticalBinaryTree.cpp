959d30af098d8634b8f10c1f2b3ed928a317fe43
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-06-14 12:51:45 +0530

    easy

--- InterviewBit/IdenticalBinaryTree.cpp
+++ InterviewBit/IdenticalBinaryTree.cpp
@@ -1,19 +1,18 @@
 /**
-* Definition for binary tree
-* struct TreeNode {
-*     int val;
-*     TreeNode *left;
-*     TreeNode *right;
-*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-* };
-*/
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 int Solution::isSameTree(TreeNode* A, TreeNode* B) {
-if(!A && !B)
-    return true;
-if(!A || !B)    
-    return false;
-return ((A -> val == B -> val) && isSameTree(A -> left, B -> left)
-    && isSameTree(A -> right, B -> right));
+    if(!A && !B)
+        return 1;
+    if(!A || !B)
+        return 0;
+    return (A -> val == B -> val) && isSameTree(A -> left, B -> left)
+    && isSameTree(A -> right, B -> right);    
 }
-
 
