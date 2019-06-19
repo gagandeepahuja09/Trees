939fa29383332dc6192301976e2433069ac4f757
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-06-19 19:40:50 +0530

    easy

--- InterviewBit/PrintAllRootToLeafPaths.cpp
+++ InterviewBit/PrintAllRootToLeafPaths.cpp
@@ -7,24 +7,28 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- 
-void paths(TreeNode* root, vector<int>& v, vector<vector<int>>& res) {
-    if(!root) {
+
+void paths(TreeNode* root, vector<int>& v, vector<vector<int>>& ret) {
+    if(!root)
+        return;
+    if(!root -> left && !root -> right) {
+        v.push_back(root -> val);
+        ret.push_back(v);
+        v.pop_back();
         return;
     }
     v.push_back(root -> val);
-    if(!root -> left && !root -> right) {
-        res.push_back(v);
-    }
-    paths(root -> left, v, res);
-    paths(root -> right, v, res);
+    paths(root -> left, v, ret);
+    paths(root -> right, v, ret);
     v.pop_back();
-}
+} 
  
 vector<vector<int> > Solution::solve(TreeNode* A) {
-    vector<vector<int>> res;
+    vector<vector<int>> ret;
+    if(!A)
+        return ret;
     vector<int> v;
-    paths(A, v, res);
-    return res;
+    paths(A, v, ret);
+    return ret;
 }
 
