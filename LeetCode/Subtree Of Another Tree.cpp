ab7381c4a5669d68fdcb213f43f7605e4839e613
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-11-16 23:34:51 +0530

    easy

--- LeetCode/Subtree Of Another Tree.cpp
+++ LeetCode/Subtree Of Another Tree.cpp
@@ -25,3 +25,24 @@ public:
         return isSameTree(s, t) || isSubtree(s -> left, t) || isSubtree(s -> right, t);
     }
 };
+
+// O(N + M)
+class Solution {
+public:
+    bool ans = false;
+    string st;
+    string f(TreeNode* root, bool ok) {
+        if(!root)
+            return "_";
+        string curr =  "(" + f(root -> left, ok) + "." + to_string(root -> val) + "." + f(root -> right, ok) + ")";
+        if(st == curr)
+            ans = true;
+        return curr;
+    }
+    
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+        st = f(t, 0);
+        f(s, 1);
+        return ans;
+    }
+};
