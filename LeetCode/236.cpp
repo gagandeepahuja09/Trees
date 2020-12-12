592d6ca384de7c8994b152a3226b5ecf2041ea39
Gagandeep Singh Ahuja <36494305+gagandeepahuja09@users.noreply.github.com>
2020-12-12 17:01:03 +0530

    very easy

---
+++ LeetCode/236.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root)
+            return root;
+        TreeNode* leftLCA = lowestCommonAncestor(root -> left, p, q);
+        TreeNode* rightLCA = lowestCommonAncestor(root -> right, p, q);
+        if (root == p || root == q)
+            return root;
+        if (leftLCA && rightLCA) {
+            return root;
+        }
+        else if (leftLCA) {
+            return leftLCA;
+        }
+        return rightLCA;
+    }
+};
